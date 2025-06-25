29f46e4eb67422179ed3e049289ba6ef96a0c5e3
Karthikeya-pydi <karthikeya.chowdary-st@officeofananyabirla.com>
2025-06-25 15:19:47 +0530

    fix

--- app/core/config.py
+++ app/core/config.py
@@ -39,7 +39,7 @@ class Settings(BaseSettings):
     IIFL_DISABLE_SSL: bool = False
     IIFL_SOURCE: str = "WEBAPI"
 
-    frontend_url: str = Field(default="http://localhost:3000", env="FRONTEND_URL")
+    frontend_url: str = "http://localhost:3000"
     class Config:
         env_file = ".env"
 
